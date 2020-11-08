df9331d6086242c1245e53a48bb60901a8fdcb92
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-08 00:07:26 -0300

    prepara as viewa para trabalhar com html

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
